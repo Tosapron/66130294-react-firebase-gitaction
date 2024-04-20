4b478604d8426d55dff54347f7424eaa140504a9
66130294@dpu.ac.th <66130294@dpu.ac.th>
2024-04-20 15:50:05 +0700

    change name title

--- src/BaseLayout.jsx
+++ src/BaseLayout.jsx
@@ -17,7 +17,7 @@ export default class BaseLayout extends Component {
               <Nav.Link href="/report">Home</Nav.Link>
               <Nav.Link href="/product">Products</Nav.Link>
             </Nav>
-            <div style={{ color: "white" }}>Siwakorn Banluesapy</div>
+            <div style={{ color: "white" }}>Tosapron Chawachaikorn</div>
           </Container>
         </Navbar>
         <p></p>
